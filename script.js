9b66ebad1ebdc5e187b247cbdc30bf19856189bc
juliakim2705-blip <juliakim2705@gmail.com>
2026-02-18 18:38:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("releaseBtn").onclick = function () {
+  document.getElementById("journalInput").value = "";
+  document.getElementById("affirmation").style.display = "block";
+};
